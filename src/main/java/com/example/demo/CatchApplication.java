e08c4b9775e943d1657b816e91607e6de6ac8126
wlgks7460 <wlgks7460@naver.com>
2024-03-19 17:34:19 +0900

    백엔드 환경구축 (#3)

---
+++ src/main/java/com/example/demo/CatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchApplication.class, args);
+	}
+
+}
