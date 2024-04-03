9c1bbfeeaaa8bd8fb5eb80f8359a16dd0b8e7f0d
hyesunlee30 <sunnylee3771@gmail.com>
2024-04-03 12:28:30 +0900

    event kafka 한정쿠펀 이벤트 발행 테스트중

---
+++ event/src/main/java/com/encore/event/common/WebAdapter.java
@@ -0,0 +1,20 @@
+package com.encore.event.common;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//@Controller 대신
+@Target({ElementType.TYPE}) // 어노테이션 적용대상 FIELD, TYPE(class, interface)
+@Retention(RetentionPolicy.RUNTIME) //클래스 파일까지 존재하며, 실행 시 사용한다. 즉, 지속 시간이 가장 길다.
+@Component
+public @interface WebAdapter {
+    @AliasFor(annotation = Component.class)
+    String value() default "";
+
+}
+
