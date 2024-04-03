296128d6de31cb01388b1db93d40512fb4a226d7
hyesunlee30 <sunnylee3771@gmail.com>
2024-04-03 12:31:55 +0900

    코드정리

--- event/src/main/java/com/encore/event/common/UseCase.java
+++ event/src/main/java/com/encore/event/common/UseCase.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-//@Controller 대신
+//@Service 대신
 @Target({ElementType.TYPE}) // 어노테이션 적용대상 FIELD, TYPE(class, interface)
 @Retention(RetentionPolicy.RUNTIME) //클래스 파일까지 존재하며, 실행 시 사용한다. 즉, 지속 시간이 가장 길다.
 @Component
