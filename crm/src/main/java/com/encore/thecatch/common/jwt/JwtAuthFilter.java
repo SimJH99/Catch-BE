16f1248857f04bd3d2a867165a45fba7383f787e
ticketpaper <dlwhdvy369@naver.com>
2024-04-05 12:25:14 +0900

    [Fix] 마케터, CS담당자 계정 생성 관련 수정 (#19)

--- crm/src/main/java/com/encore/thecatch/common/jwt/JwtAuthFilter.java
+++ crm/src/main/java/com/encore/thecatch/common/jwt/JwtAuthFilter.java
@@ -18,8 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.userdetails.User;
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Component
 @RequiredArgsConstructor
