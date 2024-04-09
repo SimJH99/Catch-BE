5789fc9da1a6f570c93080e3505aed0591a0a15d
wlgks7460 <wlgks7460@naver.com>
2024-04-09 09:30:04 +0900

    [Feat] Coupon FE 연결

--- crm/src/main/java/com/encore/thecatch/admin/repository/AdminRepository.java
+++ crm/src/main/java/com/encore/thecatch/admin/repository/AdminRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Optional<Admin> findByEmployeeNumber(String employeeNumber);
+    Optional<Admin> findByEmail(String email);
 }
--- crm/src/main/java/com/encore/thecatch/coupon/dto/CouponResDto.java
+++ crm/src/main/java/com/encore/thecatch/coupon/dto/CouponResDto.java
@@ -4,12 +4,14 @@ import com.encore.thecatch.company.domain.Company;
 import com.encore.thecatch.coupon.domain.Coupon;
 import com.encore.thecatch.coupon.domain.CouponStatus;
 import com.encore.thecatch.publish_coupon.domain.PublishCoupon;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class CouponResDto {
+    private Long id;
     private String name;
     private String code;
     private CouponStatus status;
@@ -20,6 +22,7 @@ public class CouponResDto {
 
     public static CouponResDto toCouponResDto(Coupon coupon){
         CouponResDtoBuilder builder = CouponResDto.builder();
+        builder.id(coupon.getId());
         builder.name(coupon.getName());
         builder.code(coupon.getCode());
         builder.status(coupon.getCouponStatus());
@@ -34,4 +37,7 @@ public class CouponResDto {
         Coupon coupon = publishCoupon.getCoupon();
         return CouponResDto.toCouponResDto(coupon);
     }
+
+
+
 }
