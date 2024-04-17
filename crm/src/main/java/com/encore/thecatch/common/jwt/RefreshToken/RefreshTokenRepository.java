514befd79e3c205009248a830efaa7c70c4e0ec6
ticketpaper <122894395+ticketpaper@users.noreply.github.com>
2024-04-17 15:33:50 +0900

    [Fix] RefreshTokenRepository

--- crm/src/main/java/com/encore/thecatch/common/jwt/RefreshToken/RefreshTokenRepository.java
+++ crm/src/main/java/com/encore/thecatch/common/jwt/RefreshToken/RefreshTokenRepository.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByIdAndReissueCountLessThan(String email, long count);
-    Optional<RefreshToken> findByAdmin_Id(Long adminId);
+    Optional<RefreshToken> findByAdminId(Long adminId);
 
 }
